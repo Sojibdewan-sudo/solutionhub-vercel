91e7aed1ae5ada811b46216b607c3abf1cecda1d
Sojibdewan-sudo <shaklaind@gmail.com>
2026-06-14 16:14:20 +0600

    fix: move CopyBtn to client

--- components/layout/ThemeProvider.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import { ThemeProvider as T } from 'next-themes'
-export function ThemeProvider({ children, ...p }: { children: React.ReactNode; [k: string]: any }) {
-  return <T {...p}>{children}</T>
-}
