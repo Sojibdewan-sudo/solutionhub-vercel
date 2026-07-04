e11707ea66e594b57b2b51290cb4c0f17151c25d
Sojibdewan-sudo <shaklaind@gmail.com>
2026-07-04 12:41:12 +0600

    fix adsetra

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
 import { ThemeProvider } from '@/components/layout/ThemeProvider'
+import { Analytics } from '@vercel/analytics/react'
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://solutionhub.aivoro.site'
 
@@ -29,28 +30,34 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
           </div>
         </ThemeProvider>
-{/* Google Translate Widget */}
-<div id="google_translate_element" style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: 9999 }} />
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      function googleTranslateElementInit() {
-        new google.translate.TranslateElement({
-          pageLanguage: 'en',
-          includedLanguages: 'bn,en,hi,ar,zh-CN,fr,de,es,pt,ru,ja,ko,tr,ur,id,ms,th,vi,fa,pl,nl,it,sv,he,sw,tl,el,hu,cs,ro,da,fi,no,uk,ca,hr,lt,lv,sl,sr,sk'
-          layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-          autoDisplay: false
-        }, 'google_translate_element');
-      }
-    `,
-  }}
-/>
-<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" />
 
-//#endregion
-<script async="async" data-cfasync="false" src="https://pl30199094.effectivecpmnetwork.com/37239f77d9eebf7980edae69d2426b32/invoke.js"></script>
-<div id="container-37239f77d9eebf7980edae69d2426b32"></div>
+        {/* Google Translate Widget */}
+        <div id="google_translate_element" style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: 9999 }} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function googleTranslateElementInit() {
+                new google.translate.TranslateElement({
+                  pageLanguage: 'en',
+                  includedLanguages: 'bn,en,hi,ar,zh-CN,fr,de,es,pt,ru,ja,ko,tr,ur,id,ms,th,vi,fa,pl,nl,it,sv,he,sw,tl,el,hu,cs,ro,da,fi,no,uk,ca,hr,lt,lv,sl,sr,sk',
+                  layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                  autoDisplay: false
+                }, 'google_translate_element');
+              }
+            `,
+          }}
+        />
+        <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" />
 
+        {/* Adsterra Ad */}
+        <script
+          async={true}
+          data-cfasync="false"
+          src="https://pl30199094.effectivecpmnetwork.com/37239f77d9eebf7980edae69d2426b32/invoke.js"
+        />
+        <div id="container-37239f77d9eebf7980edae69d2426b32" />
+
+        <Analytics />
       </body>
     </html>
   )
